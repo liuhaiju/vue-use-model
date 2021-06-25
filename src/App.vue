8419e257bcc77a51bd9afba2ac02c86b6a490d3e
liuhaidong <3105579283@qq.com>
2021-06-25 17:19:11 +0800

    初步创建vue包

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <router-view v-if="show"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      show: false
+    }
+  },
+  created() {
+    // var x = document.cookie
+    setTimeout(()=>{
+
+          console.log("得到的x的值话好多好多好多话好多话",window.get_address_info)
+      this.show = true
+    },1000)
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  overflow: hidden;
+  margin: 0px;
+  background-color: #ffffff;
+  font-family: Helvetica;
+  position: relative;
+}
+* {
+  /* touch-action: pan-y pan-x; */
+  touch-action:none;
+  touch-action:pan-y;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <span>Hello World!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .about-event-modifier {
+    background-color: blanchedalmond;
+  }
+  .about-event-modifier span {
+    background-color: chartreuse;
+  }
+</style>
